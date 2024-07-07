14b98a4b844cd0d1987730a915c39a6bbb6d9822
Aritra Dey <adey01027@gmail.com>
2024-07-07 20:00:29 +0530

    Modified db js file

--- backend/db.js
+++ backend/db.js
@@ -1,30 +1,30 @@
 const mongoose = require('mongoose');
-const { number } = require('zod');
 
-mongoose.connect("mongodb://localhost:27017/paytm");
-const userSchema=new mongoose.Schema({
-    name:String,
-    password:String,
-    firstName:String,
-    lastName:String
+mongoose.connect("mongodb://localhost:27017/paytm", { useNewUrlParser: true, useUnifiedTopology: true });
+
+const userSchema = new mongoose.Schema({
+    name: String,
+    password: String,
+    firstName: String,
+    lastName: String
 });
 
-const accountSchema=mongoose.Schema({
-    user:{
-        type:mongoose.Schema.Types.objectId,
-        ref:'User',
-        required:true
+const accountSchema = new mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
     },
-    balance:{
-        type:Number,
-        required:true
+    balance: {
+        type: Number,
+        required: true
     }
-})
+});
 
-const User=mongoose.model('User',userSchema);
-const Account=mongoose.model('account',accountSchema)
-module.exports=
-({
+const User = mongoose.model('User', userSchema);
+const Account = mongoose.model('Account', accountSchema);
+
+module.exports = {
     User,
     Account
-})
+};
